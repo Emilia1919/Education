a99605bdbf9da614c1145d24608bad115094e147
Tolstik_Emilia <tolstik.emilia.787888@mail.ru>
2020-10-19 19:41:01 +0300

    Lesson1

---
+++ OnlineStore/21century/21century/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _21century
+{
+    public class Constants
+    {
+        public const string TITLE = "Интернет-магазин '21 век'";
+    }
+}
