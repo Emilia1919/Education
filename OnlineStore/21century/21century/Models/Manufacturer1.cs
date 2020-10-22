c5d8efb32b4cf861d3a21935c3f079f567426615
Tolstik_Emilia <tolstik.emilia.787888@mail.ru>
2020-10-22 11:15:51 +0300

    lesson 2

---
+++ OnlineStore/21century/21century/Models/Manufacturer1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using _21century.Models.Metadata;
+
+namespace _21century.Models
+{
+    public class Manufacturer1
+    {
+        [MetadataType(typeof(ManufacturerMetadata))]
+        public partial class Manufacturer
+        {
+        }
+    }
+}
