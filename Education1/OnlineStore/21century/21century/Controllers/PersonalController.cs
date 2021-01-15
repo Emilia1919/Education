9b1bf6533cf0a7e60980c414b3281c25dd6e5557
Tolstik_Emilia <tolstik.emilia.787888@mail.ru>
2021-01-15 23:29:31 +0300

    Fasad Standart. New 3

--- Education1/OnlineStore/21century/21century/Controllers/PersonalController.cs
+++ Education1/OnlineStore/21century/21century/Controllers/PersonalController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _21century.Models;
 
 namespace _21century.Controllers
 {
@@ -11,7 +12,22 @@ namespace _21century.Controllers
         {
             public ActionResult Index()
             {
-                return View();
-            }
+            UserProfile profile = new UserProfile(User.Identity.Name);
+            return View(profile);
         }
+        public ActionResult Edit()
+        {
+            UserProfile profile = new UserProfile(User.Identity.Name);
+            return View(profile);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(FormCollection collection)
+        {
+            UserProfile profile = new UserProfile(User.Identity.Name);
+            UpdateModel(profile, collection);
+            profile.Save();
+            return RedirectToAction("Index");
+        }
+    }
 }
